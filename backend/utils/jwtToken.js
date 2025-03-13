d2a741660005bc8a2f748e38db175f92e920ba25
viswanathreddy1017 <ch.kasiviswanathreddy@gmail.com>
2025-03-13 15:52:20 -0500

    deleted secure samesite in jwt

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,8 +5,6 @@ export const generateToken =(user, message, statusCode, res)=>{
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: true,
-    sameSite: "None",
     }).json({
         success: true,
         message,
