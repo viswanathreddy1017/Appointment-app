2bec3072c44d60f5c6ed58b4acd66e2018a3cbae
viswanathreddy1017 <ch.kasiviswanathreddy@gmail.com>
2025-03-13 00:40:19 -0500

    database name change

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => { 
     mongoose
         .connect(process.env.MONGO_URI, {
-            dbName: "APPOINTMENT MANAGEMENT APPLICATION",
+            dbName: "ApplicationManagementApp",
         })
         .then(() => {
             console.log('MongoDB Connected');
