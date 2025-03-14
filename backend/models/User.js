7ab4e423f5fc86690742b39a5c3463f579e2616c
viswanathreddy1017 <ch.kasiviswanathreddy@gmail.com>
2025-03-14 12:08:34 -0500

    bcrypt changed from bcryptjs

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
-import bcryptjs from "bcryptjs";  
+import bcrypt from "bcrypt";  
 import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
@@ -20,11 +20,11 @@ userSchema.pre("save", async function (next) {
   if(!this.isModified("password")){
     next();
   }
-  this.password = await bcryptjs.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 userSchema.methods.comparePassword = async function(enteredPassword){
-  return await bcryptjs.compare(enteredPassword, this.password);
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 userSchema.methods.generateJsonWebToken = function(){
